1d9dcaac2357a1e3f9cb4903e8825477c0779943
timoer-b <timoer@everbloom.finance>
2025-03-13 11:38:12 +0100

    Routing & different pages

--- frontend/README.md
+++ frontend/README.md
@@ -8,6 +8,12 @@ npm create vite@latest frontend --template react
 
 Install dependencies with `npm i`.
 
+Additional dependencies that were installed:
+
+```
+npm install react-router-dom axios
+```
+
 ## Run
 
 The frontend can be run with `npm run dev`.
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import './App.css'
 import {Home} from "./pages/Home.jsx";
+import {Link, Route, Routes} from "react-router-dom";
+import {AddBook} from "./pages/AddBook.jsx";
 
 const App = () => {
 
   return (
     <>
-      <Home/>
+      <nav>
+        <Link to={"/"} className={'nav-btn'}>Home</Link>
+        <Link to={"/add"} className={'nav-btn'}>Add Book</Link>
+      </nav>
+
+
+      <Routes>
+        <Route path={'/'} element={<Home/>}/>
+        <Route path={'/add'} element={<AddBook/>}/>
+      </Routes>
     </>
   )
 }
